755dec850db7f029abd13bcc21f69bc86bcf1d5a
Ctrlalton03 <altonsimpson22@gmail.com>
2024-01-28 09:44:24 -0500

    New webpage for socials

--- Main.js
+++ Main.js
@@ -4,4 +4,15 @@ document.getElementById('myVideo').addEventListener('ended',myHandler,false);
             // What you want to do after the event
             document.getElementById('myVideo').style.display="none";
             document.getElementById('videoEnd').style.display="block";
-        }
+        };
+
+document.addEventListener("visibilitychange", function() {
+            if (document.visibilityState === 'visible') {
+              // Tab is now visible, play the video
+              playVideo();
+            } else {
+              // Tab is not visible, pause the video
+              playVideo();
+            }
+          });
+        
